05fab07ce6a65e3120138c51153ebeb3140dc68b
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-06-24 10:44:07 -0700

    [Fixes #11454] Fix encoding bug on prerendering

--- src/Components/Server/src/Circuits/CircuitPrerenderer.cs
+++ src/Components/Server/src/Circuits/CircuitPrerenderer.cs
@@ -4,11 +4,12 @@
 using System;
 using System.Linq;
 using System.Runtime.ExceptionServices;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Http.Features;
 
 namespace Microsoft.AspNetCore.Components.Server.Circuits
 {
@@ -16,11 +17,15 @@ namespace Microsoft.AspNetCore.Components.Server.Circuits
     {
         private static object CircuitHostKey = new object();
         private static object NavigationStatusKey = new object();
+        private static readonly JsonSerializerOptions _jsonSerializationOptions =
+            new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
         private readonly CircuitFactory _circuitFactory;
         private readonly CircuitRegistry _registry;
 
-        public CircuitPrerenderer(CircuitFactory circuitFactory, CircuitRegistry registry)
+        public CircuitPrerenderer(
+            CircuitFactory circuitFactory,
+            CircuitRegistry registry)
         {
             _circuitFactory = circuitFactory;
             _registry = registry;
@@ -71,8 +76,14 @@ namespace Microsoft.AspNetCore.Components.Server.Circuits
                 Prerendered = true
             });
 
+            var record = JsonSerializer.ToString(new PrerenderedComponentRecord(
+                    circuitHost.CircuitId.Replace("--", "__"), // We need to do this due to the fact that -- is not allowed within HTML comments and HTML doesn't encode '-'.
+                    circuitHost.Renderer.Id,
+                    renderResult.ComponentId),
+                _jsonSerializationOptions);
+
             var result = (new[] {
-                $"<!-- M.A.C.Component:{{\"circuitId\":\"{circuitHost.CircuitId}\",\"rendererId\":\"{circuitHost.Renderer.Id}\",\"componentId\":\"{renderResult.ComponentId}\"}} -->",
+                $"<!-- M.A.C.Component: {record} -->",
             }).Concat(renderResult.Tokens).Concat(
                 new[] {
                     $"<!-- M.A.C.Component: {renderResult.ComponentId} -->"
@@ -162,6 +173,22 @@ namespace Microsoft.AspNetCore.Components.Server.Circuits
             }
 
             return result;
+        }
+
+        private struct PrerenderedComponentRecord
+        {
+            public PrerenderedComponentRecord(string circuitId, int id, int componentId)
+            {
+                CircuitId = circuitId;
+                RendererId = id;
+                ComponentId = componentId;
+            }
+
+            public string CircuitId { get; }
+
+            public int RendererId { get; }
+
+            public int ComponentId { get; }
         }
 
         private class CircuitNavigationStatus
